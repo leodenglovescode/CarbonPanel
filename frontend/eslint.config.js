002f513903c8cd03df721b5f37dec78995dc732e
leodenglovescode <71826908+leodenglovescode@users.noreply.github.com>
2026-03-26 12:05:26 +0800

    chore: add ESLint configuration and dependencies for TypeScript and Vue support
    - Added ESLint and related dependencies to package.json
    - Created eslint.config.js with recommended configurations for JavaScript, TypeScript, and Vue
    - Configured parser options and rules for TypeScript and Vue files

---
+++ frontend/eslint.config.js
@@ -0,0 +1,37 @@
+import js from '@eslint/js'
+import tseslint from '@typescript-eslint/eslint-plugin'
+import tsParser from '@typescript-eslint/parser'
+import vue from 'eslint-plugin-vue'
+import vueParser from 'vue-eslint-parser'
+
+export default [
+  {
+    ignores: ['dist/**', 'node_modules/**'],
+  },
+  js.configs.recommended,
+  ...vue.configs['flat/recommended'],
+  {
+    files: ['**/*.ts', '**/*.vue'],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: tsParser,
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+        extraFileExtensions: ['.vue'],
+      },
+      globals: {
+        document: 'readonly',
+        localStorage: 'readonly',
+        window: 'readonly',
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      ...tseslint.configs.recommended.rules,
+      'vue/multi-word-component-names': 'off',
+    },
+  },
+]
